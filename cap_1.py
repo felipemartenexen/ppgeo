5f821692f6731b3033420d2a53657067b0ec2e57
Felipe Martenexen <felipe.martenexen@gmail.com>
2024-02-29 23:46:32 -0300

    Update cap_1.py

--- cap_1.py
+++ cap_1.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
-
+import seaborn as sns
+import matplotlib.ticker as mticker
 #%%Preparo dos dados
 df = pd.read_csv(r"C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\areas_rix_fire_lulc_year_v2.csv")
 
@@ -118,9 +119,78 @@ df_soi_fire = pd.merge(df_fire_month, df_soi_index, on=['year', 'month'], how='l
 
 print(df_soi_fire)
 
+#%% Gráfico Mensal
 
+df_fire_month_grouped = df_fire_month.groupby('month')['area'].sum().reset_index()
+total_area_yearly = df_fire_month_grouped['area'].sum()
+df_fire_month_grouped['percentage'] = (df_fire_month_grouped['area'] / total_area_yearly) * 100
+
+month_id = {
+    'Jan': 1,
+    'Fev': 2,
+    'Mar': 3,
+    'Abr': 4,
+    'Mai': 5,
+    'Jun': 6,
+    'Jul': 7,
+    'Ago': 8,
+    'Set': 9,
+    'Out': 10,
+    'Nov': 11,
+    'Dez': 12 
+}
+
+df_fire_month_grouped['month_id'] = df_fire_month_grouped['month'].map(month_id)
+
+# Ordenando os dados pelo mês numericamente para garantir a sequência de Janeiro a Dezembro
+df_fire_month_grouped.sort_values('month_id', inplace=True)
+# Criando o gráfico de barras com porcentagem
+# Recriando o gráfico com a correção para garantir que os rótulos estejam na ordem correta
+fig, ax1 = plt.subplots(figsize=(14, 8))
+sns.barplot(x='month', y='area', data=df_fire_month_grouped, color='red', ax=ax1)
+ax1.set_title('Área Queimada Mensal')
+ax1.set_xlabel('Mês')
+ax1.set_ylabel('Área Queimada (km²)')
+
+# Corrigindo a posição dos rótulos
+for i, (index, row) in enumerate(df_fire_month_grouped.iterrows()):
+    ax1.text(i, row['area'], f"{row['area']/1000:.2f}k\n({row['percentage']:.2f}%)", color='black', ha="center", va='bottom')
+
+# Ajustando o eixo Y à esquerda para mostrar os valores em 'k' (milhares)
+ticks_loc = ax1.get_yticks().tolist()
+ax1.yaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
+ax1.set_yticklabels(['{:.0f}k'.format(x/1000) for x in ticks_loc])
+
+plt.tight_layout()
+plt.show()
+
+#%% Distribuição Fogo - Mês - Ano
+
+df_fire_month['month_id'] = df_fire_month['month'].map(month_id)
+df_fire_month.sort_values('month_id', inplace=True, ascending=True)
+
+# Criando a figura
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plotando o heatmap
+sns.heatmap(data=df_fire_month.pivot_table(index="month_id", columns="year", values="area"),
+            annot=False, fmt=".2f", ax=ax)
+
+# Definindo labels
+ax.set_xlabel("Ano")
+ax.set_ylabel("Mês")
+
+# Rotacionando labels do eixo x
+plt.xticks(rotation=45)
+
+# Adicionando título
+plt.title("Heatmap da Área")
+
+# Mostrando o gráfico
+plt.show()
 #%%Export
 
 output_file_path = r"C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\df_fire_month_v1.csv"
 
 df_fire_month.to_csv(output_file_path, index=True)
+# %%
