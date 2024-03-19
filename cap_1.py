28ebd34c59ebde432c216f6539f18dedff75e8a9
Felipe Martenexen <felipe.martenexen@gmail.com>
2024-03-18 22:43:50 -0300

    Update cap_1.py

--- cap_1.py
+++ cap_1.py
@@ -1,10 +1,13 @@
 #%%
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from scipy import stats
 import numpy as np
 import seaborn as sns
 import matplotlib.ticker as mticker
+from matplotlib.ticker import FuncFormatter
+
 #%%Preparo dos dados
 df = pd.read_csv(r"C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\areas_rix_fire_lulc_year_v2.csv")
 
@@ -30,6 +33,10 @@ id_to_class = {
     11: 'Campo Alagado e Área Pantanosa',
     12: 'Formação Campestre',
     15: 'Pastagem',
+    24: 'Área Urbanizada',
+    29: 'Afloramento Rochoso',
+    30: 'Mineração',
+    33: "Corpo D'água",
     39: 'Soja',
     41: 'Outras Lavouras Temporárias',
     48: 'Outras Lavouras Perenes'
@@ -51,11 +58,16 @@ df['NM_MUN'] = df['CD_MUN_NUM'].map(municipality_code_to_name)
 
 df['class_name'] = df['class'].map(id_to_class)
 
+df['class_lev1'] = df['class_name'].map(id_to_lev1)
+
 print(df.head())
 
 #%% Distribuição Anual
 
 df_fire_year = df.groupby('year')['area'].sum().reset_index()
+
+# Cálculo da média
+media_area_queimada = df_fire_year["area"].mean()
 
 # Cálculo da tendência linear
 slope, intercept, r_value, p_value, std_err = stats.linregress(df_fire_year.index, df_fire_year["area"])
@@ -63,10 +75,16 @@ slope, intercept, r_value, p_value, std_err = stats.linregress(df_fire_year.inde
 # Adicionando a tendência linear ao DataFrame
 df_fire_year['trend'] = intercept + slope * df_fire_year.index
 
-# Plotando os dados e a tendência
+# Configurando a fonte globalmente
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['font.size'] = 12
+
+# Plotando os dados, a tendência e a linha da média
 plt.figure(figsize=(10, 6))
 plt.plot(df_fire_year["year"], df_fire_year["area"], label='Área Queimada (km²)', marker='o', color='red')
 plt.plot(df_fire_year["year"], df_fire_year["trend"], label='Tendência Linear', linestyle='--', color='black')
+plt.axhline(y=media_area_queimada, color='blue', linestyle='-', label='Média Área Queimada')
+
 plt.title('Análise Temporal das Áreas Queimadas (1985-2022)')
 plt.xlabel('Ano')
 plt.ylabel('Área Queimada (km²)')
@@ -320,7 +338,12 @@ cores_classes = {
     'Outras Lavouras Temporárias': '#f54ca9',
     'Pastagem': '#edde8e',
     'Silvicultura': '#7a5900',
-    'Soja': '#f5b3c8'
+    'Soja': '#f5b3c8', 
+    'Agricultura': '#E974ED',
+    'Área Urbanizada': '#d4271e',
+    "Corpo Dágua": '#0000FF',
+    'Afloramento Rochoso':'#ffaa5f',
+    'Mineração': '#9c0027'
 }
 
 # Função para formatar os valores do eixo Y com separador de milhar como ponto
@@ -373,11 +396,7 @@ for tick in ax.get_yticklabels():
 plt.tight_layout()
 
 plt.show()
-#%%Export
 
-output_file_path = r"C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\df_normalized_by_class.csv"
-
-area_normalizada.to_csv(output_file_path, index=True)
 # %% Gráfico Rosca Uso e Cobertura do Solo
 class_name_custom = {
     "Campo Alagado e Área Pantanosa": "Campo Alagado e Área Pantanosa",
@@ -434,3 +453,305 @@ fig.gca().add_artist(centre_circle)
 
 plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 plt.show()
+
+
+# %% Gráfico Precipitação Área Queimada
+
+precipitation_df = pd.read_csv(r'C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\mean_precipitation_rix_1985_2022.csv')
+
+# Agrupar os dados somente por 'year', somando as áreas, para corresponder aos dados de precipitação
+total_area_by_year_df = df.groupby('year')['area'].sum().reset_index()
+
+# Unir os conjuntos de dados com base em 'year'
+merged_df = pd.merge(total_area_by_year_df, precipitation_df[['year', 'precipitation']], on='year', how='inner')
+
+# Selecionar as colunas de interesse: 'year', 'precipitation', e 'area'
+final_df = merged_df[['year', 'precipitation', 'area']]
+
+# Exibir as primeiras linhas do dataframe final
+final_df.head()
+
+# Configurar o gráfico com estilos mais avançados
+fig, ax1 = plt.subplots(figsize=(12, 7))
+
+# Melhorar a aparência com seaborn
+sns.set_style("whitegrid")
+
+# Definição da fonte (você precisará definir 'font_path' caso o try acima falhe)
+plt.rcParams['font.family'] = 'Times New Roman'  
+
+# Função para formatar os valores do eixo Y com separador de milhar como ponto
+def custom_formatter(x, pos):
+    return f'{x:,.0f}'.replace(',', '.')
+
+# Eixo para 'area'
+color = 'tab:red'
+ax1.set_xlabel('Ano', fontsize=12)  
+ax1.set_ylabel('Área Queimada(km²)', color=color, fontsize=12) 
+lns1 = ax1.plot(final_df['year'], final_df['area'], color=color, label='Área (km²)', marker='o', linestyle='-')
+ax1.tick_params(axis='y', labelcolor=color, labelsize=12)
+ax1.tick_params(axis='x', labelsize=12)
+
+# Eixo para 'precipitation'
+ax2 = ax1.twinx()
+color = 'tab:blue'
+ax2.set_ylabel('Precipitação (mm)', color=color, fontsize=12)  
+lns2 = ax2.plot(final_df['year'], final_df['precipitation'], color=color, label='Precipitação (mm)', linestyle='--')
+ax2.tick_params(axis='y', labelcolor=color, labelsize=12)
+
+# Combinar legendas
+lns = lns1 + lns2
+labs = [l.get_label() for l in lns]
+plt.legend(lns, labs, loc='upper center',bbox_to_anchor=(0.5, -0.1), fontsize=12, frameon=False)
+
+# Formatando o eixo y para usar ponto como separador de milhar
+ax1.yaxis.set_major_formatter(mticker.FuncFormatter(custom_formatter))
+ax2.yaxis.set_major_formatter(mticker.FuncFormatter(custom_formatter))
+
+
+# Mostrar o gráfico
+plt.show()
+
+#%% Extensão
+
+acumulado = pd.read_csv(r"C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\freq_rix_1985_2022_v2.csv")
+
+acumulado['freq'] = acumulado['class'].divide(100).astype(int)
+
+acumulado['territory'] = acumulado['territory'].astype(int)
+
+acumulado['class_id'] = acumulado['class'].mod(100)
+
+acumulado = acumulado.drop(columns=['system:index', '.geo', 'class'])
+
+fire_extension_by_year = acumulado.groupby(['year'])['area'].sum().reset_index()
+
+print(fire_extension_by_year.head())
+
+# Configurando as fontes para Times New Roman, tamanho 12
+matplotlib.rcParams['font.family'] = 'Times New Roman'
+matplotlib.rcParams['font.size'] = 12
+
+# Função para formatar os valores do eixo Y com separador de milhar como ponto
+def custom_formatter(x, pos):
+    return f'{x:,.0f}'.replace(',', '.')
+
+# Usando Python e Pandas para gerar um gráfico de área acumulada
+ax = fire_extension_by_year.plot(kind='area', x='year', y='area', figsize=(12, 6), alpha=0.4, color='red', label='Extensão Área Queimada')
+
+plt.xlabel('Ano')
+
+plt.ylabel('Área Queimada (km²)')
+plt.grid(False)
+plt.xticks(rotation=45, fontname='Times New Roman', fontsize=12)
+
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=2, frameon=False)
+
+# Formatando o eixo y para usar ponto como separador de milhar
+ax.yaxis.set_major_formatter(mticker.FuncFormatter(custom_formatter))
+
+plt.show()
+
+#%% Uso do Solo
+use_land_file = pd.read_csv(r'C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\areas_lulc_rix_1985_2022.csv')
+
+use_land = use_land_file.drop(columns=['system:index', '.geo'])
+
+use_land = use_land.groupby(['year', 'class'])['area'].sum().reset_index()
+
+use_land['class_name'] = use_land['class'].map(id_to_class)
+
+print(use_land.head())
+
+# Definindo uma função para formatar o eixo y
+def formatador(x, pos):
+    return f'{x:,.0f}'.replace(',', '.')
+
+formatter = FuncFormatter(formatador)
+
+# Agrupando as categorias especificadas em 'Agricultura'
+categorias_agricultura = ['Outras Lavouras Temporárias', 'Outras Lavouras Perenes', 'Soja', 'Silvicultura']
+dados_agrupados = use_land.copy()
+dados_agrupados['class_name'] = dados_agrupados['class_name'].apply(lambda x: 'Agricultura' if x in categorias_agricultura else x)
+
+# Pivotando os dados novamente com o agrupamento atualizado
+dados_pivot_agrupados = dados_agrupados.pivot_table(values='area', index='year', columns='class_name', aggfunc='sum')
+
+# Criando o gráfico de linha com ajustes de legenda e cor
+plt.figure(figsize=(12, 8))
+for coluna in dados_pivot_agrupados.columns:
+    plt.plot(dados_pivot_agrupados.index, dados_pivot_agrupados[coluna], label=coluna, color=cores_classes.get(coluna, 'gray'))
+
+plt.xlabel('Ano', fontdict={'fontname': 'Times New Roman', 'fontsize': 12})
+plt.ylabel('Área (km²)', fontdict={'fontname': 'Times New Roman', 'fontsize': 12})
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=5, fontsize=12, title_fontsize=12, frameon=False)
+plt.grid(False)
+plt.gca().yaxis.set_major_formatter(formatter)
+plt.xticks(rotation=45, fontname='Times New Roman', fontsize=12)
+plt.yticks(fontname='Times New Roman', fontsize=12)
+plt.tight_layout(rect=[0, 0.03, 1, 0.95]) # Ajustando para a legenda não sobrepor o gráfico
+
+# Formatando o eixo y para usar ponto como separador de milhar
+ax.yaxis.set_major_formatter(mticker.FuncFormatter(custom_formatter))
+
+plt.show()
+
+#%% Recorrencia de Queimada
+rec_fire = pd.read_csv(r"C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\freq_rix_1985_2022_v2.csv")
+
+rec_fire['freq'] = rec_fire['class'].divide(100).astype(int)
+
+rec_fire['territory'] = rec_fire['territory'].astype(int)
+
+rec_fire['class_id'] = rec_fire['class'].mod(100)
+
+rec_fire = rec_fire.drop(columns=['system:index', '.geo', 'class'])
+
+rec_fire_2022 = rec_fire.query('year == 2022')
+
+rec_fire_2022 = rec_fire_2022.groupby(['freq', 'class_id'])['area'].sum().reset_index()
+
+rec_fire_2022['class_name'] = rec_fire_2022['class_id'].map(id_to_class)
+
+total_area_km2 = rec_fire_2022['area'].sum()
+
+print(total_area_km2)
+
+# Definir a fonte para Times New Roman, tamanho 12
+plt.rcParams.update({
+    'font.family': 'serif',
+    'font.serif': 'Times New Roman',
+    'font.size': 12
+})
+
+# Função para formatar os valores do eixo Y com separador de milhar como ponto
+def custom_formatter(x, pos):
+    return f'{x:,.0f}'.replace(',', '.')
+
+formatter = FuncFormatter(formatador)
+
+# Categorizar frequências acima de 10 como "Acima de 10"
+rec_fire_2022['freq_category'] = rec_fire_2022['freq'].apply(lambda x: 'Acima de 10' if x > 10 else str(x))
+
+# Agrupar os dados pela nova categoria 'freq_category'
+grouped_data_new = rec_fire_2022.groupby('freq_category').agg({'area':'sum'}).reset_index()
+grouped_data_new['percent_of_total_area'] = (grouped_data_new['area'] / grouped_data_new['area'].sum()) * 100
+
+# Ordenar os dados para a plotagem
+grouped_data_new['freq_category'] = pd.Categorical(grouped_data_new['freq_category'], categories=list(map(str, range(1, 11))) + ['Acima de 10'], ordered=True)
+grouped_data_new = grouped_data_new.sort_values('freq_category')
+
+# Configurar cores usando o mapa de cores YlOrBr
+colors = cm.YlOrBr(np.linspace(0.3, 0.9, len(grouped_data_new)))
+
+# Criar figura e eixo para o gráfico de barras
+fig, ax1 = plt.subplots(figsize=(10, 5))
+
+# Plotar as barras para a área
+bars = ax1.bar(grouped_data_new['freq_category'], grouped_data_new['area'], color=colors, label='Área (km²)')
+ax1.set_xlabel('Recorrência')
+ax1.set_ylabel('Área Queimada (km²)')
+ax1.tick_params(axis='y')
+
+# Criar eixo secundário para as porcentagens
+ax2 = ax1.twinx()
+ax2.plot(grouped_data_new['freq_category'], grouped_data_new['percent_of_total_area'], color='red', marker='o', linestyle='-', label='Porcentagem do total')
+ax2.set_ylabel('Porcentagem (%)')
+ax2.tick_params(axis='y')
+
+# Ajustar o posicionamento dos rótulos de porcentagem
+for i, txt in enumerate(grouped_data_new['percent_of_total_area']):
+    ax2.annotate(f'{txt:.2f}%', 
+                 (grouped_data_new['freq_category'][i], grouped_data_new['percent_of_total_area'][i]),
+                 textcoords="offset points", 
+                 xytext=(0,5), 
+                 ha='center')
+    
+plt.tight_layout()
+
+# Formatando o eixo y para usar ponto como separador de milhar
+ax1.yaxis.set_major_formatter(mticker.FuncFormatter(custom_formatter))
+
+plt.show()
+
+#%% Recorrencia por Uso
+
+# Substituir os nomes das classes específicas por 'Agricultura'
+rec_fire_2022['class_name_aggregated'] = rec_fire_2022['class_name'].replace({
+    'Outras Lavouras Temporárias': 'Agricultura',
+    'Outras Lavouras Perenes': 'Agricultura',
+    'Soja': 'Agricultura',
+    'Silvicultura': 'Agricultura'
+})
+
+# Agrupar freq maiores que 10 em 'maior que 10'
+rec_fire_2022['freq_aggregated'] = rec_fire_2022['freq'].apply(lambda x: 'Acima de 10' if x > 10 else str(x))
+
+# Assegurar que os valores de 'freq_aggregated' estejam ordenados corretamente
+rec_fire_2022['freq_aggregated'] = pd.Categorical(rec_fire_2022['freq_aggregated'], categories=[str(x) for x in range(1, 11)] + ['Acima de 10'], ordered=True)
+
+# Agrupar os dados por 'class_name_aggregated' e 'freq_aggregated', somando a área
+grouped_data = rec_fire_2022.groupby(['class_name_aggregated', 'freq_aggregated']).agg({'area': 'sum'}).reset_index()
+
+# Configurar a fonte para Times New Roman, tamanho 12
+plt.rcParams.update({
+    'font.family': 'serif',
+    'font.serif': 'Times New Roman',
+    'font.size': 12
+})
+
+# Função para formatar os valores do eixo Y com separador de milhar como ponto
+def custom_formatter(x, pos):
+    return f'{x:,.0f}'.replace(',', '.')
+
+formatter = FuncFormatter(formatador)
+
+# Definir cores específicas para cada classe
+color_map = {
+    'Formação Florestal': '#1f8d49',
+    'Formação Campestre': '#d6bc74',
+    'Campo Alagado e Área Pantanosa': '#519799',
+    'Formação Savânica': '#7dc975',
+    'Agricultura': '#E974ED',
+    'Área Urbanizada': '#d4271e',
+    'Corpo Dágua': '#0000FF',
+    'Afloramento Rochoso': '#ffaa5f',
+    'Mineração': '#9c0027',
+    'Pastagem': '#edde8e'
+}
+
+# Criar a figura e o eixo para o gráfico
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plotar um gráfico de linha para cada classe agregada
+for class_name, group in grouped_data.groupby('class_name_aggregated'):
+    if class_name in color_map:  # Verificar se a classe tem cor definida
+        ax.plot(group['freq_aggregated'], group['area'], label=class_name, color=color_map[class_name])
+
+# Configurar os rótulos, título do gráfico e ajustar a legenda
+ax.set_xlabel('Recorrência de Fogo')
+ax.set_ylabel('Área Queimada(km²)')
+
+# Ajustar a legenda para baixo e distribuí-la horizontalmente
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False)
+
+# Formatando o eixo y para usar ponto como separador de milhar
+ax.yaxis.set_major_formatter(mticker.FuncFormatter(custom_formatter))
+
+plt.grid(False)
+plt.tight_layout()
+plt.show()
+
+
+#%% Export
+acumulado['city_name'] = acumulado['territory'].map(municipality_code_to_name)
+
+acumulado['class_name'] = acumulado['class_id'].map(id_to_class)
+
+print(acumulado)
+
+output_file_path = r"C:\Users\luiz.felipe\Desktop\FLP\Mestrado\etapa_1\data\areas_rix_fire_level1_v1.csv"
+
+df.to_csv(output_file_path, index=True)
+
+# %%
